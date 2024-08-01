e1888b4fd8cb8fcf6c65e75d36214fbb9d0f57f4
hazn <aitakprojects@gmail.com>
2024-08-01 16:05:01 +0200

    peopen-3: add more class definitions to third notebook

--- notebooks/third_refactored_approach.py
+++ notebooks/third_refactored_approach.py
@@ -6,7 +6,7 @@ app = marimo.App(width="medium")
 
 @app.cell
 def __(mo):
-    mo.md("""# Imports""")
+    mo.md("""# Import""")
     return
 
 
@@ -22,8 +22,21 @@ def __():
 
     # dspy imports
     import dspy
+    from dspy.predict import Predict
     from dspy.datasets import DataLoader
-    return DataLoader, Enum, List, Optional, dataclass, dspy, field, mo
+    from dspy.teleprompt.teleprompt import Teleprompter
+    return (
+        DataLoader,
+        Enum,
+        List,
+        Optional,
+        Predict,
+        Teleprompter,
+        dataclass,
+        dspy,
+        field,
+        mo,
+    )
 
 
 @app.cell
@@ -39,7 +52,19 @@ def __(mo):
 
 
 @app.cell
-def __(Enum):
+def __(
+    DataLoader,
+    Enum,
+    Literal,
+    Mapping,
+    Optional,
+    Predictor,
+    Teleprompter,
+    dataclass,
+    dspy,
+    field,
+    load,
+):
     class TrainingLanguage(Enum):
         GERMAN = "de"
         FRENCH = "fr"
@@ -50,25 +75,12 @@ def __(Enum):
     class ModelType(Enum):
         TEXT = "text"
         CHAT = "chat"
-    return ModelType, TrainingLanguage
 
 
-@app.cell
-def __(
-    DataLoader,
-    Literal,
-    Mapping,
-    ModelType,
-    Optional,
-    TrainingLanguage,
-    dataclass,
-    dspy,
-    field,
-):
     @dataclass(frozen=True)
-    class Model:
+    class LanguageModel:
         """
-        Defines which model to use
+        Defines which LM to use and configures dspy to use the model
         """
 
         name: str = field(default="")
@@ -91,13 +103,14 @@ def __(
         Hyperparameters for the training, load once at the start of the program, never changed
         """
 
-        # if data should be truncate
+        # if data should be truncated
         is_truncation_required: bool = field(default=False)
         # how much of the training data should be used
         training_set_limit: Optional[int] = field(default=None)
         # dataset training language
         language: TrainingLanguage = field(default=TrainingLanguage.ALL)
 
+        # @todo: implement
         # not implemented yet:
         # phoenix_metadata: Optional[dict] = field(default=None)
         # model: str = field(default=None)
@@ -148,41 +161,131 @@ def __(
                 pass
 
             object.__setattr__(self, "data", dataset)
-    return Dataset, HyperParams, Model
 
 
-@app.cell
-def __(mo):
-    mo.md(r"""## Loading the data""")
-    return
+    @dataclass(frozen=True)
+    class Metrics:
+        """
+        Defines calculation of eval metrics
+        """
+
+        @staticmethod
+        def _calculate_bert_score(
+            pred: str,
+            ground_truth: str,
+        ) -> float:
+            """Computes and returns the average f1 BERTScore based on untokenized inputs"""
+            bertscore = load("bertscore")
+            bert_scores = bertscore.compute(
+                predictions=[pred],
+                references=[ground_truth],
+                model_type="bert-base-multilingual-cased",
+                lang=["de", "fr", "it"],
+            )
+            # bertscore can return multiple values, so we average them
+            return sum(bert_scores["f1"]) / len(bert_scores["f1"])
+
+        @staticmethod
+        def get_metrics(example, prediction, trace=None):
+            # function signature is important to be compatible with dspy optimizers
+            # @todo: implement more metrics
+            # @todo: make `regeste` not hard coded?
+            return Metrics._calculate_bert_score(
+                pred=prediction.regeste, ground_truth=example.regeste
+            )
 
 
-@app.cell
-def __(Dataset, HyperParams, TrainingLanguage):
-    # @todo: change so this is read from outside the code
-    hyperparameters = HyperParams(
-        is_truncation_required=False,
-        training_set_limit=None,
-        language=TrainingLanguage.GERMAN,
+    class Network(dspy.Module):
+        """
+        Defines the model structure and training steps
+        """
+
+        prediction_pipeline: Predictor = None
+
+        # @todo: split up
+        class DecisionSummarizationSignature(dspy.Signature):
+            """Ziel: Generiere eine Regeste basierend auf einem Schweizer Gerichtsurteils.\nHintergrund: Ein Schweizer Gerichtsurteil setzt sich aus Sachverhalt, Erwägungen und Dispositiv zusammen. Die Regeste dient als Kurzzusammenfassung und beinhaltet Leitsätze des Urteils. Nur Leitentscheide haben eine Regeste.\nAnweisung:\n1. Sachverhalt: Lies und verstehe den gegebenen Sachverhalt.\n2. Erwägungen: Analysiere die Erwägungen, um die Hauptargumente und Gründe zu identifizieren.\n3. Dispositiv: Beachte das Dispositiv, da es das endgültige Urteil enthält.\n4. Erstelle die Regeste: Die Regeste sollte aus drei sehr kurzen Teilen bestehen: a. Zitiere die wichtigsten relevanten Artikelziffern (ohne den Artikeltitel). b. Nenne kurze, relevante, deskriptive Keywords, über die Thematik des Falls. c. Formuliere einen sehr kurzen Fliesstext, der die wichtigsten Erwägungen zitiert und kurz zusammenfasst.\nOutput: Die Regeste sollte eine klare und strukturierte Kurzzusammenfassung des Urteils bieten, die aus zitierten Artikeln, Keywords und einem sehr kurzen Fliesstext besteht."""
+
+            text = dspy.InputField()
+            regeste = dspy.OutputField()
+
+        def __init__(self):
+            super().__init__()
+            # @todo: change up prediction pipeline after splitting
+            self.prediction_pipeline = dspy.ChainOfThought(
+                self.DecisionSummarizationSignature()
+            )
+
+        def forward(self, text: str):
+            assert prediction_pipeline
+            # @todo: implement phoenix attributes/metadata here @see first notebook
+            # @todo: make this universal so `text` isn't hardcoded?
+            # text parameter is equivalent to the lds dataset `text` input
+            return prediction_pipeline(text=text)
+
+
+    @dataclass(frozen=True)
+    class Trainer:
+        """ """
+
+        metrics: Metrics
+        network: Network
+        data: Dataset
+        optimizer: Teleprompter
+        # @todo implement for optimizing based on hyperparams as well
+        # hyperparameters: HyperParams
+
+        def optimize(self):
+            _save_model()
+
+        def _save_model(self):
+            pass
+    return (
+        Dataset,
+        HyperParams,
+        LanguageModel,
+        Metrics,
+        ModelType,
+        Network,
+        Trainer,
+        TrainingLanguage,
     )
-    dataset = Dataset(hyperparameters).data
-    return dataset, hyperparameters
 
 
 @app.cell
 def __(mo):
-    mo.md(r"""## Loading the model""")
+    mo.md(
+        r"""
+
+        ## Instantiation of all objects
+        """
+    )
     return
-
-
-@app.cell
-def __(Model):
-    model = Model().model
-    return model,
 
 
 @app.cell
 def __():
+    # @todo: define everything outside of this notebook, with all parameters load into from outside this file
+
+
+    # LanguageModel:
+    # HyperParams:
+    # Dataset:
+    # Metrics:
+    # Network
+    # Trainer:
+    return
+
+
+@app.cell
+def __(mo):
+    mo.md(r"""# Functional core""")
+    return
+
+
+@app.cell
+def __(mo):
+    mo.md(r"""## Testing the model with a call""")
     return
 
 
