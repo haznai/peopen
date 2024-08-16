17423f128c3c191bd7ed207962250ad391f110b2
hazn <aitakprojects@gmail.com>
2024-08-16 15:23:17 +0200

    peop-4: improve structured output prompt

--- src/scripts/structured_data_from_broshures.py
+++ src/scripts/structured_data_from_broshures.py
@@ -2,6 +2,62 @@ from typing_extensions import Literal
 from pydantic import BaseModel
 from openai import OpenAI
 
+# ---- I/O ----
+prompt = r"""
+Du bist ein hilfreicher Chatbot der halb-strukturierte OCR-Texte in JSON-Strukturen umwandelt.
+Ich gebe dir den Text einer schweizerischen Abstimmungsbroschüre. Der Text wurde mit OCR extrahiert und ist somit nicht in einem 100% korrekten Format. Die Abstimmungsbroschüre beschreibt Schweizer Volksinitiativen. Für jede beschriebene Initiative liefert die Broschüre detaillierte Informationen für die Volksinitiativen, Argumente seitens des Initiativkomitees, des Bundesrates und Parlaments.
+
+Aufgrund des OCR-Verfahrens ist der Text nicht verlässlich strukturiert. Bitte hilf mir, dem Text eine JSON-Struktur zu geben. Die Struktur dargestellt mit zwei Beispiel Volksiniativen:
+
+```json
+{
+  "volksiniativen": [
+    // Kommentar: Muster für eine Volksinitiative 1
+    {
+      "titel": "Prämien-Entlastungs-Initiative",
+      "im_detail": [
+        {
+          "ueberschrift": "Ausgangslage",
+          "inhalt": "Wer in der Schweiz krank ist, erhält die nötige medizinische Behandlung. Seit 1996 übernimmt die obligatorische Krankenversicherung die Kosten dafür. Die Krankenversicherung wird über die Krankenkassenprämien und Kostenbeteiligungen (Franchise, Selbstbehalt, Spitalkostenbeitrag) finanziert. Die Kosten der Krankenversicherung sind in den letzten Jahrzehnten stark gestiegen. Um sie zu decken, mussten die Prämien entsprechend erhöht werden. Die Prämien stiegen im Verhältnis deutlich mehr als die Löhne"
+        },
+        {
+          "ueberschrift": "Prämienverbilligung",
+          "inhalt": "Die Prämien werden pro Person und unabhängig von derEinkommenshöhe bestimmt. Die Kantone sind verpflichtet, die Prämien der Versicherten in bescheidenen wirtschaftlichen Verhältnissen zu verbilligen. Die Kantone erhalten dazu vomBund einen Beitrag. Der Mittelstand profitiert jedoch nicht oder nur teilweise von dieser Verbilligung und wird darum von den steigenden Prämien zunehmend stark belastet"
+        },
+        // Kommentar: Weitere Paragraphen hier ...
+      ],
+      "argumenteKomitee": [
+        {
+          "ueberschrift": "Worum geht es?",
+          "inhalt": "Die Krankenkassenprämien steigen seit Jahren und reissen ein immer grösseres Loch in unser Portemonnaie. Bis zu 15 000 Franken: So viel zahlt heute eine vierköpfige Familie pro Jahr für die Krankenkasse. Die Prämienexplosion ist aber nur ein Spiegelbild der steigenden Kosten im Gesundheitswesen. Um das Problem nachhaltig zu lösen, braucht es jetzt die Kostenbremse."
+        },
+        {
+          "ueberschrift": "Drohen Rationierungen?",
+          "inhalt": "Nein. Im Gegenteil: Die Initiative will, dass alle Akteur eendlich Verantwortung für die Kostenexplosion übernehmen und der interne Verteilkampf zulasten der Prämienzahlenden aufhört. Während Hausärztinnen, Kinderärzte und Pflegende schon heute die Lasten des Systems tragen, bereichern sich andere schamlos."
+        }
+        // Kommentar: Weitere Argumente hier ...
+      ],
+      "empfehlungKomitee": "ja",
+      "argumenteBundesrat": [
+        {
+          "ueberschrift": "Richtige Diagnose, falsches Mittel",
+          "inhalt": "Die Initiative greift ein wichtiges Problem auf: Die Kosten in der obligatorischen Krankenversicherung steigen zu stark. Es gibt ineffiziente Strukturen und es werden mehr Behandlungen durchgeführt, als medizinisch nötig wären. Die Initiative ist aber zu starr: Sie bindet das erlaubte Kostenwachstum einseitig an die Entwicklung der Löhne und der Wirtschaft. Damit werden nachvollziehbare Gründe für das Kostenwachstum ausgeblendet, beispielsweise der medizinische Fortschritt oder die Alterung der Bevölkerung."
+        }
+        // Kommentar: Weitere Argumente hier ...
+      ],
+      "empfehlungBundesrat": "nein"
+    },
+    // Kommentar: Muster für eine Volksinitiative 2
+    {
+      "titel": "Kostenbremse-Initiative"
+      // Kommentar: Weitere Felder hier ...
+    }
+  ]
+}
+```
+"""
+
+# ---- Schema of the structured data ----
 class Argument(BaseModel):
     ueberschrift: str
     inhalt: str
@@ -21,84 +77,14 @@ class Volksiniative(BaseModel):
 class StructuredData(BaseModel):
     volksiniativen: list[Volksiniative]
 
+
+# ---- OpenAI API call ----
 client = OpenAI()
 
 # todo: give examples
 completion = client.beta.chat.completions.parse(
     model="gpt-4o-2024-08-06",
-    messages=[{"role": "system", "content": r"""
-            Du bist ein hilfreicher Chatbot der halb-strukturierte OCR-Texte in JSON-Strukturen umwandelt.
-            ch gebe dir den Text einer schweizerischen Abstimmungsbroschüre. Der Text wurde mit OCR extrahiert und ist somit nicht in einem ganz richtigen Format. Die Abstimmungsbroschüre beschreibt Schweizer Volksinitiativen. Für jede beschriebene Initiative liefert die Broschüre detaillierte Informationen für die Volksinitiativen, Argumente seitens des Initiativkomitees, des Bundesrates und Parlaments.
-
-            Aufgrund des OCR-Verfahrens ist der Text nicht verlässlich strukturiert. Bitte hilf mir, dem Text eine JSON-Struktur zu geben. Die Struktur soll wie folgt aussehen (Musterinhalt):
-
-            ```json
-            {
-	"volksiniativen": [
-		// Beispiel Volksiniative 1
-		{
-			"titel": "Prämien-Entlastungs-Initiative",
-			// "Im Detail" info über die Iniative
-			"im_detail": [
-				{
-					// Jeder Paragraph hat eine überschrift im text
-					"ueberschrift": "Ausgangslage",
-					"inhalt": "Wer in der Schweiz krank ist, erhält die nötige medizinische Behandlung. Seit 1996 übernimmt die obligatorische Krankenversicherung die Kosten dafür. Die Krankenversicherung wird über die Krankenkassenprämien und Kostenbeteiligungen (Franchise, Selbstbehalt, Spitalkostenbeitrag) finanziert. Die Kosten der Krankenversicherung sind in den letzten Jahrzehnten stark gestiegen. Um sie zu decken, mussten die Prämien entsprechend erhöht werden. Die Prämien stiegen im Verhältnis deutlich mehr als die Löhne"
-				},
-				{
-					"ueberschrift": "Prämienverbilligung",
-					"inhalt": "Die Prämien werden pro Person und unabhängig von derEinkommenshöhe bestimmt. Die Kantone sind verpflichtet, die Prämien der Versicherten in bescheidenen wirtschaftlichen Verhältnissen zu verbilligen. Die Kantone erhalten dazu vomBund einen Beitrag. Der Mittelstand profitiert jedoch nicht oder nur teilweise von dieser Verbilligung und wird darum von den steigenden Prämien zunehmend stark belastet"
-				},
-				...
-			]
-			// Argumentenliste des Komitees
-			"argumenteKomitee": [
-				{
-					// jedes argument hat eine überschrift im text
-					"ueberschrift": "Worum geht es?",
-					// das argument
-					"inhalt": "Die Krankenkassenprämien steigen seit Jahren und reissen ein immer grösseres Loch in unser Portemonnaie. Bis zu 15 000 Franken: So viel zahlt heute eine vierköpfige Familie pro Jahr für die Krankenkasse. Die Prämienexplosion ist aber nur ein Spiegelbild der steigenden Kosten im Gesundheitswesen. Um das Problem nachhaltig zu lösen, braucht es jetzt die Kostenbremse."
-				},
-				{
-					"ueberschrift": "Drohen
-
-            Rationierungen?",
-					"inhalt": "Nein. Im Gegenteil: Die Initiative will, dass alle Akteur eendlich Verantwortung für die Kostenexplosion übernehmen und der interne Verteilkampf zulasten der Prämienzahlenden
-            aufhört. Während Hausärztinnen, Kinderärzte und Pflegende
-            schon heute die Lasten des Systems tragen, bereichern sich
-            andere schamlos."
-
-				},
-				...
-			]
-			// die broschüre enthält die Info ob das komitee eine Ja oder Nein stimme empfiehlt
-			"empfehlungKomitee": "ja",
-						// Argumentenliste des Bundesrates und Parlaments
-			"argumenteBundesrat": [
-				{
-					// jedes argument hat eine überschrift im text
-					"ueberschrift": "Richtige Diagnose, falsches Mittel",
-					// das argument
-					"inhalt": "Die Initiative greift ein wichtiges Problem auf: Die Kosten in der obligatorischen Krankenversicherung steigen zu stark. Es gibt ineffiziente Strukturen und es werden mehr Behandlungen durchgeführt, als medizinisch nötig wären. Die Initiative ist
-            aber zu starr: Sie bindet das erlaubte Kostenwachstum einseitig
-            an die Entwicklung der Löhne und der Wirtschaft. Damit
-            werden nachvollziehbare Gründe für das Kostenwachstum
-            ausgeblendet, beispielsweise der medizinische Fortschritt oder die Alterung der Bevölkerung."
-				},
-				...
-			]
-			// die broschüre enthält die Info ob derBunesrat  eine Ja oder Nein stimme empfiehlt
-			"empfehlungBundesrat": "nein"
-		},
-		{
-			"titel": "Kostenbremse-Initiative"
-			...
-		}
-            }
-            ```
-
-            Es interessieren nur die Argumente, alle anderen Informationen können ignoriert werden.
-            """},
+    messages=[{"role": "system", "content": prompt},
         {"role": "user", "content": r"""
             ---------- OCR-Text von der Broschüre
             Im Detail **Volksinitiative «Für ein besseres**
