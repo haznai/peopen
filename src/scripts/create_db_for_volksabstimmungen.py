5b66f98fe62c1c00668e2d0ff5f293796ed03533
hazn <aitakprojects@gmail.com>
2024-08-25 18:30:17 +0200

    peop-4: change name of script

--- src/scripts/create_db_for_volksabstimmungen.py
+++ src/scripts/create_db_for_volksabstimmungen.py
@@ -1,16 +1,20 @@
-# Counts the number of things to vote on in each broshure
-
 import pandas as pd
+import sqlite3
 
 # load csv into pd dataframe
 # "Datum" is the first column and a date
 df = pd.read_csv("../../data/processed/table_of_each_volksabstimmung_since_new_broshure.csv")
 
-
 # convert "Datum" to datetime, then into iso format string
 df["Datum"] = pd.to_datetime(df["Datum"], format="mixed").dt.strftime("%Y-%m-%d")
 
 
-# count the number of volksinitiatives in each broshure
-# by "Datum" column, sorted
-df.groupby("Datum").count()["art"]
+# save data in database for later use
+# Connect to SQLite database
+conn = sqlite3.connect('volksabstimmungen.db')
+
+# Write DataFrame to SQLite database
+df.to_sql('volksabstimmungen', conn, if_exists='replace', index=False)
+
+# Close the connection
+conn.close()
